f93116c8a6f2445522b4d5bc7616330df7d52503
saram <Salshahrani153@gmail.com>
2021-09-28 16:03:15 +0300

    done

--- Lab3Playground.playground/Contents.swift
+++ Lab3Playground.playground/Contents.swift
@@ -1,11 +1,59 @@
 
 // Task 1
+var ID:String? = nil
+print(ID as Any)
+
+var num = 19
+print(num)
 
 // Task 2
+var name:[String]=["sara","deema", "asma", "aisha", "reema"]
+for name in name {
+    print(name)
+}
 
 // Task 3
+func resturant (dis: Float, price: Float) -> Float {
+    return dis*price
+ }
+resturant(dis: 0.5, price: 100)
+resturant(dis: 0.25, price: 200)
 
 // Task 4
-
+var stuGPA : Int = 3
+if stuGPA > 3 {
+    print("nice;)")
+}
+else if stuGPA < 3 {
+    print("not nice :(")
+}
+if stuGPA == 3 {
+    print("nice and not nice :)")
+}
 // Task 5
+var bouns : Int = 11000
+if bouns > 10000 {
+    print("you will travel to Paris and London.")
+}
+else if
+    bouns > 5000{
+    print("you will travel to Tokyo.")
+}
+else if
+    bouns < 9999 {
+    print("you will travel to Tokyo.")
+}
+else if
+    bouns > 1000 {
+    print("you will travel to Bangkok.")
+}
+else if
+    bouns < 4999 {
+    print("you will travel to Bangkok.")
+}
+else if
+    bouns < 1000 {
+    print("you just stay home.")
+}
+
 
