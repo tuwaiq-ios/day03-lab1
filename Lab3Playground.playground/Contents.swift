9e1ffd920a6da51427bbc97b699069a81aac06b1
PC <alahmryn5@gmail.com>
2021-09-28 17:25:35 +0300

    Lab finished

--- Lab3Playground.playground/Contents.swift
+++ Lab3Playground.playground/Contents.swift
@@ -1,11 +1,51 @@
+var a:Int?
+a =  nil
+if a != nil {
+    print(a)
+}
 
-// Task 1
+a = 5
+if a != 5 {
+    print(a)
+}
+let names = ["noor", "malik", "sara", "bander", "noha"]
+for names in names {print(names)
+    
+}
 
-// Task 2
+func priceDi(price : Double, dis : Double) -> Double {
+    let priceDi = (price - (price * (dis/100)))
+    return priceDi
+}
+print(priceDi(price: 100, dis:50))
+print(priceDi(price: 200, dis:25))
+var studGPA = 3
+if studGPA > 3 {
+    print("nice")
+}else if studGPA < 3{print("not nice")}
+else if studGPA == 3{
+    print("nice and not nice")
+}
+var bouns = 1234
+  if bouns >= 10000{
+      print("you will  travel to paris and london.")}
+else if (bouns >= 5000){
+    print("you will travel to tokyo")}
+else if (bouns >= 1000){
+    print("you will travel to Bangkok.")}
+else{
+    print("you just stay home.")
+}
 
-// Task 3
+    
 
-// Task 4
+    
+    
+            
+    
 
-// Task 5
+
+    
+
+
 
