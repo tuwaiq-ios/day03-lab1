a8087c99a0c6d8f854761256ff7fcf1aaeedc4ad
sana alshahrani <me-sa51@hotmail.com>
2021-09-28 16:27:16 +0300

    Sana Alshahrani

--- Lab3Playground.playground/Contents.swift
+++ Lab3Playground.playground/Contents.swift
@@ -1,11 +1,64 @@
 
 // Task 1
+var name:String? = nil
+var name2:String?
+print (name as Any)
+var student = 20
+    print(student)
+
 
 // Task 2
+    var names = ["sana","tasneem","deema","sara","ruba"]
+for name in names {
+  print(name)
+}
 
 // Task 3
+func shirt(price: Float, discount: Float) -> Float {
+    return discount*price
+}
+shirt(price:100, discount:0.5)
+shirt(price:200, discount: 0.25)
+    
 
 // Task 4
+var studentGPA : Int = 2
+
+if studentGPA > 3 {
+    print ("nice;)")
+}
+else if
+    studentGPA < 3 {
+    print ("not nice:(")
+}
+    else if
+        studentGPA == 3 {
+        print ("nice and not nice")
+    }
+
+
 
 // Task 5
+
+var Bouns : Int =  11000
+if Bouns > 10000 {
+print ("you will travel to Paris and London:")
+}
+else if
+    5000 < Bouns && Bouns < 9999 {
+    print ("you will travel to Tokyo:")
+}
+else if
+    1000 < Bouns && Bouns < 4999 {
+        print ("you will to Bangkok:")
+}
+if Bouns < 1000 {
+    print ("you just stay home:")
+}
+
+
+    
+
+
+
 
