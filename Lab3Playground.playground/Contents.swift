55355a0daf1e47bd382acd588d631c43097efaf8
fawazmm79 <fawazmm79>
2021-09-28 16:49:36 +0300

    day 3 down

--- Lab3Playground.playground/Contents.swift
+++ Lab3Playground.playground/Contents.swift
@@ -1,6 +1,6 @@
 
 // Task 1
-
+print("\n- - - - Task 1 - - - -")
 var name: String? = nil
 if name == nil {
     print("Hello AA")
@@ -9,24 +9,29 @@ if name != nil {
     print("Hello BB")
 }
 
-// Task 2
 
+// Task 2
+print("\n- - - - Task 2 - - - -")
 let NameList = ["Fawaz","Ali","Ahmad","majeed","saad"]
 for Name in NameList {
     print(Name)
 }
 
-// Task 3
 
-func pd (cp:Double , dis:Double)-> Double{
-    let x:Double = cp - cp * dis
-    return x
+// Task 3
+print("\n- - - - Task 3 - - - -")
+func calculateDiscount(price:Double , discount:Double) -> Double {
+  let result: Double = price - price * discount
+  // let result2 = price - price * discount
+  return result
 }
-print (pd(cp:100 , dis:0.5))
-print (pd(cp:200 , dis:0.25))
+
+print(calculateDiscount(price: 100, discount: 0.5))
+print(calculateDiscount(price: 200, discount: 0.25))
+
 
 // Task 4
-
+print("\n- - - - Task 4 - - - -")
 var GPA:Int = 4
 if (GPA > 3){
     print("nice")
@@ -38,18 +43,27 @@ else if (GPA >= 3){
     print("nice and not nice")
 }
 
+
 // Task 5
 
+print("\n- - - - Task 5 - - - -")
+// If Version
 var travel:Int = 10000
-if (travel >= 1000){
+
+if (travel >= 10000){
     print("travel to Paris and London")
 }
 else if (travel > 5000 && travel < 9999){
     print("travel to Toky")
 }
-else if (travel > 1000 && travel < 4999){
+else if (travel > 10000 && travel < 4999){
     print("travel to Bangkok")
 }
-else if (travel < 1000){
+else if (travel < 10000){
     print("stay home")
 }
+
+// Switch Version
+
+
+
